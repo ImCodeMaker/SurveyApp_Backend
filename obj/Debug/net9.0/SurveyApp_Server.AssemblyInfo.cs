909c53bb4ad96a21cfc2d04ef026614232931b67
ImCodeMaker <xxjoshi2006@gmail.com>
2025-04-09 14:36:05 -0400

    added more files

--- obj/Debug/net9.0/SurveyApp_Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/SurveyApp_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84fd5e9070d3f7f55f3c14117dff81b7534b8c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
