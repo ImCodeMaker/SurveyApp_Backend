08f1657a7264d1f2a3106826f4bbcb5c45262bb7
ImCodeMaker <xxjoshi2006@gmail.com>
2025-04-20 14:30:20 -0400

    Added last migration.

--- obj/Debug/net9.0/SurveyApp_Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/SurveyApp_Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d1307de1a5da3383eae2846c86e528eb9dba9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc14959e5c65b4a789a3850ca8e8753c39fc645")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurveyApp_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
